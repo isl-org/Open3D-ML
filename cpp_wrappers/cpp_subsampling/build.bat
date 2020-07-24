94498bec627fe8a900b655ebb3f1c2e0e1fa58b9
YilingQiao <yilingq@umd.edu>
2020-07-24 10:57:50 -0400

    batchers

---
+++ cpp_wrappers/cpp_subsampling/build.bat
@@ -0,0 +1,5 @@
+@echo off
+py setup.py build_ext --inplace
+
+
+pause
---
+++ ml3d/torch/datasets/concat_batcher.py
@@ -0,0 +1,172 @@
+
+
+# Common libs
+import time
+import numpy as np
+import pickle
+import torch
+import yaml
+from multiprocessing import Lock
+
+
+# OS functions
+from os import listdir
+from os.path import exists, join, isdir
+
+# Dataset parent class
+from torch.utils.data import Sampler, get_worker_info
+
+
+
+class SemanticKittiCustomBatch:
+    """Custom batch definition with memory pinning for SemanticKitti"""
+    def __init__(self, input_list):
+
+        # Get rid of batch dimension
+        input_list = input_list[0]['data']
+
+        # Number of layers
+        L = int(input_list[0])
+
+        # Extract input tensors from the list of numpy array
+        ind = 1
+        self.points = [torch.from_numpy(nparray) for nparray in input_list[ind:ind+L]]
+        ind += L
+        self.neighbors = [torch.from_numpy(nparray) for nparray in input_list[ind:ind+L]]
+        ind += L
+        self.pools = [torch.from_numpy(nparray) for nparray in input_list[ind:ind+L]]
+        ind += L
+        self.upsamples = [torch.from_numpy(nparray) for nparray in input_list[ind:ind+L]]
+        ind += L
+        self.lengths = [torch.from_numpy(nparray) for nparray in input_list[ind:ind+L]]
+        ind += L
+        self.features = torch.from_numpy(input_list[ind])
+        ind += 1
+        self.labels = torch.from_numpy(input_list[ind])
+        ind += 1
+        self.scales = torch.from_numpy(input_list[ind])
+        ind += 1
+        self.rots = torch.from_numpy(input_list[ind])
+        ind += 1
+        self.frame_inds = torch.from_numpy(input_list[ind])
+        ind += 1
+        self.frame_centers = torch.from_numpy(input_list[ind])
+        ind += 1
+        self.reproj_inds = input_list[ind]
+        ind += 1
+        self.reproj_masks = input_list[ind]
+        ind += 1
+        self.val_labels = input_list[ind]
+
+        return
+
+    def pin_memory(self):
+        """
+        Manual pinning of the memory
+        """
+
+        self.points = [in_tensor.pin_memory() for in_tensor in self.points]
+        self.neighbors = [in_tensor.pin_memory() for in_tensor in self.neighbors]
+        self.pools = [in_tensor.pin_memory() for in_tensor in self.pools]
+        self.upsamples = [in_tensor.pin_memory() for in_tensor in self.upsamples]
+        self.lengths = [in_tensor.pin_memory() for in_tensor in self.lengths]
+        self.features = self.features.pin_memory()
+        self.labels = self.labels.pin_memory()
+        self.scales = self.scales.pin_memory()
+        self.rots = self.rots.pin_memory()
+        self.frame_inds = self.frame_inds.pin_memory()
+        self.frame_centers = self.frame_centers.pin_memory()
+
+        return self
+
+    def to(self, device):
+
+        self.points = [in_tensor.to(device) for in_tensor in self.points]
+        self.neighbors = [in_tensor.to(device) for in_tensor in self.neighbors]
+        self.pools = [in_tensor.to(device) for in_tensor in self.pools]
+        self.upsamples = [in_tensor.to(device) for in_tensor in self.upsamples]
+        self.lengths = [in_tensor.to(device) for in_tensor in self.lengths]
+        self.features = self.features.to(device)
+        self.labels = self.labels.to(device)
+        self.scales = self.scales.to(device)
+        self.rots = self.rots.to(device)
+        self.frame_inds = self.frame_inds.to(device)
+        self.frame_centers = self.frame_centers.to(device)
+
+        return self
+
+    def unstack_points(self, layer=None):
+        """Unstack the points"""
+        return self.unstack_elements('points', layer)
+
+    def unstack_neighbors(self, layer=None):
+        """Unstack the neighbors indices"""
+        return self.unstack_elements('neighbors', layer)
+
+    def unstack_pools(self, layer=None):
+        """Unstack the pooling indices"""
+        return self.unstack_elements('pools', layer)
+
+    def unstack_elements(self, element_name, layer=None, to_numpy=True):
+        """
+        Return a list of the stacked elements in the batch at a certain layer. If no layer is given, then return all
+        layers
+        """
+        if element_name == 'points':
+            elements = self.points
+        elif element_name == 'neighbors':
+            elements = self.neighbors
+        elif element_name == 'pools':
+            elements = self.pools[:-1]
+        else:
+            raise ValueError('Unknown element name: {:s}'.format(element_name))
+
+        all_p_list = []
+        for layer_i, layer_elems in enumerate(elements):
+
+            if layer is None or layer == layer_i:
+
+                i0 = 0
+                p_list = []
+                if element_name == 'pools':
+                    lengths = self.lengths[layer_i+1]
+                else:
+                    lengths = self.lengths[layer_i]
+
+                for b_i, length in enumerate(lengths):
+
+                    elem = layer_elems[i0:i0 + length]
+                    if element_name == 'neighbors':
+                        elem[elem >= self.points[layer_i].shape[0]] = -1
+                        elem[elem >= 0] -= i0
+                    elif element_name == 'pools':
+                        elem[elem >= self.points[layer_i].shape[0]] = -1
+                        elem[elem >= 0] -= torch.sum(self.lengths[layer_i][:b_i])
+                    i0 += length
+
+                    if to_numpy:
+                        p_list.append(elem.numpy())
+                    else:
+                        p_list.append(elem)
+
+                if layer == layer_i:
+                    return p_list
+
+                all_p_list.append(p_list)
+
+        return all_p_list
+
+
+
+class ConcatBatcher(object):
+    """docstring for BaseBatcher"""
+    def __init__(self, device):
+        super(ConcatBatcher, self).__init__()
+        self.device = device
+
+    def collate_fn(self, batch):
+        batching_result = SemanticKittiCustomBatch(batch)
+        batching_result.to(self.device)
+        #print(batching_result['data']['features'].size())
+        #exit()
+        return {'data':batching_result}
