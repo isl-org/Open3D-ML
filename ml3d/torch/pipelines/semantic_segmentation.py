20d280fa25731c8f40c81b68faf071502f010d0d
YilingQiao <yilingq@umd.edu>
2020-07-28 22:29:59 -0400

    delete accuracy and iou in pipeline file

--- ml3d/torch/pipelines/semantic_segmentation.py
+++ ml3d/torch/pipelines/semantic_segmentation.py
@@ -31,69 +31,6 @@ logging.basicConfig(
 )
 log = logging.getLogger(__name__)
 
-def intersection_over_union(scores, labels):
-    r"""
-        Compute the per-class IoU and the mean IoU # TODO: complete doc
-
-        Parameters
-        ----------
-        scores: torch.FloatTensor, shape (B?, C, N)
-            raw scores for each class
-        labels: torch.LongTensor, shape (B?, N)
-            ground truth labels
-
-        Returns
-        -------
-        list of floats of length num_classes+1 (last item is mIoU)
-    """
-    num_classes = scores.size(-2) 
-    predictions = torch.max(scores, dim=-2).indices
-
-    ious = []
-
-    for label in range(num_classes):
-        pred_mask = predictions == label
-        labels_mask = labels == label
-        iou = (pred_mask & labels_mask).float().sum() 
-        iou = iou / (pred_mask | labels_mask).float().sum()
-        ious.append(iou.cpu().item())
-    ious.append(np.nanmean(ious))
-    return ious
-
-
-def accuracy(scores, labels):
-    r"""
-        Compute the per-class accuracies and the overall accuracy 
-
-        Parameters
-        ----------
-        scores: torch.FloatTensor, shape (B?, C, N)
-            raw scores for each class
-        labels: torch.LongTensor, shape (B?, N)
-            ground truth labels
-
-        Returns
-        -------
-        list of floats of length num_classes+1 
-        (last item is overall accuracy)
-    """
-    num_classes = scores.size(-2) 
-
-    predictions = torch.max(scores, dim=-2).indices
-
-    accuracies = []
-
-    accuracy_mask = predictions == labels
-    for label in range(num_classes):
-        label_mask = labels == label
-        per_class_accuracy = (accuracy_mask & label_mask).float().sum()
-        per_class_accuracy /= label_mask.float().sum()
-        accuracies.append(per_class_accuracy.cpu().item())
-    # overall accuracy
-    accuracies.append(accuracy_mask.float().mean().cpu().item())
-    #accuracies = np.array(accuracies)
-    return accuracies
-
 
 class SemanticSegmentation():
     def __init__(self, model, dataset, cfg):
