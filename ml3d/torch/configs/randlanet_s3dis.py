92c730948afc7dcdcda0e099211b1fa28de0b846
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2020-07-31 12:05:06 +0530

    modified config

--- ml3d/torch/configs/randlanet_s3dis.py
+++ ml3d/torch/configs/randlanet_s3dis.py
@@ -9,7 +9,7 @@ model       = dict(
     sub_sampling_ratio = [4, 4, 4, 4, 2],
     num_sub_points = [4096 * 10 // 4, 4096 * 10 // 16, 4096 * 10 // 64, 4096 * 10 // 256, 4096 * 10 // 512],
 
-    d_in            = 3,
+    d_in            = 6,
     d_feature       = 8,
     d_out           = [16, 64, 128, 256, 512],
 
@@ -44,4 +44,5 @@ dataset = dict(
     test_area_idx = 3, # Area_0 to Area_6
     class_weights = [3370714, 2856755, 4919229, 318158, 375640, 478001, 974733,
                                       650464, 791496, 88727, 1284130, 229758, 2272837],
+    ignored_label_inds = []
     )
