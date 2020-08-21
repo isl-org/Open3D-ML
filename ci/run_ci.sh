0d40e42fa717b3fa0b7053bd809d68f8bd5834b8
Benjamin Ummenhofer <benjaminum@gmail.com>
2020-08-21 18:37:02 +0200

    added some code to the ci script for testing on local machine

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #
 #
-# 1. clone Open3D-ML repo
-#
+# 1. clone Open3D-ML repo (done by the CI system, this is just for testing locally)
+git clone git@github.com:intel-isl/Open3D-ML.git
+PATH_TO_OPEN3D_ML=$(pwd)/Open3D-ML
 #
 # 2. clone and build Open3D
 # For now we have to clone the o3dml_integration branch of open3d
-# git clone --recursive --branch o3dml_integration  https://github.com/intel-isl/Open3D.git
+git clone --recursive --branch o3dml_integration  https://github.com/intel-isl/Open3D.git
 #
 # install deps + tf + torch
 # 
@@ -15,12 +16,36 @@
 #       -DOPEN3D_ML_ROOT=path_to_open3d_ml_repo_root \
 #       -DBUILD_TENSORFLOW_OPS=ON \
 #       -DBUILD_PYTORCH_OPS=ON \
-#       -DBUILD_RPC_INTERFACE=OFF
+#       -DBUILD_GUI=OFF \
+#       -DBUILD_RPC_INTERFACE=OFF \
 #       -DBUILD_UNIT_TESTS=OFF \
 #       -DBUILD_BENCHMARKS=OFF \
 #       -DBUILD_EXAMPLES=OFF \
 # 
+mkdir Open3D/build
+pushd Open3D/build
+cmake -DBUNDLE_OPEN3D_ML=ON \
+      -DOPEN3D_ML_ROOT=$PATH_TO_OPEN3D_ML \
+      -DBUILD_TENSORFLOW_OPS=ON \
+      -DBUILD_PYTORCH_OPS=ON \
+      -DBUILD_GUI=OFF \
+      -DBUILD_RPC_INTERFACE=OFF \
+      -DBUILD_UNIT_TESTS=OFF \
+      -DBUILD_BENCHMARKS=OFF \
+      -DBUILD_EXAMPLES=OFF \
+      ..
+
 # 3. install wheel
+make -j install-pip-package
+
 #
-# 4. run examples/tests in the Open3D-ML repo (these do not exist yet)
+# 4. run examples/tests in the Open3D-ML repo outside of the repo directory to 
+#    make sure that the installed package works.
 #
+popd
+mkdir test_workdir
+pushd test_workdir
+mv $PATH_TO_OPEN3D_ML/examples .
+python examples/ttest_integration.py
+python examples/test_semantic_seg.py
+popd
