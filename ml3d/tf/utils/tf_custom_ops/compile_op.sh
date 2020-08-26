cdf989d5cbcd47103eecb328157f84c867c85297
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2020-08-26 16:40:25 +0530

    create symlink if not present

--- ml3d/tf/utils/tf_custom_ops/compile_op.sh
+++ ml3d/tf/utils/tf_custom_ops/compile_op.sh
@@ -4,6 +4,35 @@
 TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
+# Create a symlink if libtensorflow_framework is not present
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ ${machine} = "Mac" ]; then
+
+	framework="${TF_LIB}/libtensorflow_framework.dylib"
+
+	if ! [ -f $framework ]; then
+		ln "${TF_LIB}/libtensorflow_framework.2.dylib" $framework
+	fi
+
+else
+	framework="${TF_LIB}/libtensorflow_framework.so"
+
+	if ! [ -f $framework ]; then
+		echo "not exists"
+		ln "${TF_LIB}/libtensorflow_framework.so.2" $framework
+	fi
+
+fi
+
+
 # Neighbors op
 g++ -std=c++11 -shared tf_neighbors/tf_neighbors.cpp tf_neighbors/neighbors/neighbors.cpp cpp_utils/cloud/cloud.cpp -o tf_neighbors.so -fPIC -I$TF_INC -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 g++ -std=c++11 -shared tf_neighbors/tf_batch_neighbors.cpp tf_neighbors/neighbors/neighbors.cpp cpp_utils/cloud/cloud.cpp -o tf_batch_neighbors.so -fPIC -I$TF_INC -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
