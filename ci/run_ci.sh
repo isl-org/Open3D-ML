06d6299460de8fca2ac5f8ef40c7c855348de7e2
YilingQiao <yilingq@umd.edu>
2020-09-09 10:37:26 -0400

    change back ci

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -7,23 +7,23 @@ TENSORFLOW_VER="2.3.0"
 TORCH_GLNX_VER=("1.5.0+cu101" "1.6.0+cpu")
 YAPF_VER="0.30.0"
 
-# set -euo pipefail
+set -euo pipefail
 
 # 1. Prepare the Open3D-ML repo and install dependencies
 export PATH_TO_OPEN3D_ML=$(pwd)
 # the build system of the main repo expects a master branch. make sure master exists
-# git checkout -b master || true
-# pip install -r requirements.txt
-# echo $PATH_TO_OPEN3D_ML
+git checkout -b master || true
+pip install -r requirements.txt
+echo $PATH_TO_OPEN3D_ML
 cd ..
-# python -m pip install -U Cython
+python -m pip install -U Cython
 
 
-# #
-# # 2. clone Open3D and install dependencies
-# # For now we have to clone the o3dml_integration branch of open3d
-# #
-# git clone --recursive --branch master  https://github.com/intel-isl/Open3D.git
+#
+# 2. clone Open3D and install dependencies
+# For now we have to clone the o3dml_integration branch of open3d
+#
+git clone --recursive --branch master  https://github.com/intel-isl/Open3D.git
 
 ./Open3D/util/install_deps_ubuntu.sh assume-yes
 python -m pip install -U tensorflow==$TENSORFLOW_VER
@@ -48,7 +48,7 @@ cmake -DBUNDLE_OPEN3D_ML=ON \
       ..
 
 # 4. Build and install wheel
-make -j8 install-pip-package
+make -j"$NPROC" install-pip-package
 
 #
 # 5. run examples/tests in the Open3D-ML repo outside of the repo directory to
