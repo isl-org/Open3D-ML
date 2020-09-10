9a14c11ef80ec2eb98e9eacc8bf1363cbf320058
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2020-09-10 13:29:41 +0530

    add semantickitti download script

---
+++ scripts/download_datasets/download_semantickitti.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+  
+if [ "$#" -ne 1 ]; then
+    echo "Please, provide the base directory to store the dataset."
+    exit 1
+fi
+
+if ! command -v unzip &> /dev/null
+then
+    echo "Error: unzip could not be found. Please, install it to continue"
+    exit
+fi
+
+BASE_DIR="$1"/SemanticKitti
+
+mkdir -p $BASE_DIR
+
+export url_data="http://www.cvlibs.net/download.php?file=data_odometry_velodyne.zip"
+export url_label="http://semantic-kitti.org/assets/data_odometry_labels.zip"
+
+wget -c -N -O $BASE_DIR'/data_odometry_labels.zip' $url_label
+wget -c -N -O $BASE_DIR'/data_odometry_velodyne.zip'  $url_data
+
+cd $BASE_DIR
+
+unzip data_odometry_velodyne.zip
+unzip data_odometry_labels.zip
+
+mkdir zip_files
+mv data_odometry_labels.zip zip_files
+mv data_odometry_velodyne.zip zip_files
