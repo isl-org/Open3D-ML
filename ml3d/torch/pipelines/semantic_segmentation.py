81f163d556d24726a4e4fd915e07496ecf7bac55
YilingQiao <yilingq@umd.edu>
2020-09-12 22:35:11 -0400

    change config

--- ml3d/torch/pipelines/semantic_segmentation.py
+++ ml3d/torch/pipelines/semantic_segmentation.py
@@ -188,7 +188,6 @@ class SemanticSegmentation(BasePipeline):
             for idx, inputs in enumerate(tqdm(train_loader, desc='training')):
 
                 results = model(inputs['data'])
-                print(results.size())
                 loss, gt_labels, predict_scores = model.get_loss(
                     Loss, results, inputs, device)
 
