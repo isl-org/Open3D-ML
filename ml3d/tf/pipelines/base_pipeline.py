9181f4dd5f3ef9037b7425154d04a8d51fb0d6d6
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2020-09-16 23:48:11 +0530

    typo

--- ml3d/tf/pipelines/base_pipeline.py
+++ ml3d/tf/pipelines/base_pipeline.py
@@ -36,7 +36,7 @@ class BasePipeline(object):
         dataset_name = dataset.name if dataset is not None else ''
         self.cfg.logs_dir = join(
             self.cfg.main_log_dir,
-            model.__class__.__name__ + '_' + dataset_name + '_torch')
+            model.__class__.__name__ + '_' + dataset_name + '_tf')
         make_dir(self.cfg.logs_dir)
 
     def get_loss(self):
