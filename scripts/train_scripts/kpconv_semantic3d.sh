02bf40425c034311429f3e9bb139fc63e4247a95
YilingQiao <yilingq@umd.edu>
2020-09-27 01:55:18 -0400

    log

--- scripts/train_scripts/kpconv_semantic3d.sh
+++ scripts/train_scripts/kpconv_semantic3d.sh
@@ -10,4 +10,5 @@ fi
 
 cd ../..
 python scripts/semseg.py $1 -c ml3d/configs/kpconv_semantic3d.yml \
---dataset_path /export/share/Datasets/Semantic3D
+--dataset_path /export/share/Datasets/Semantic3D_2000 \
+--pipeline.train_sum_dir /export/share/projects/open3d_ml/training_logs/v0.11/
--- scripts/train_scripts/randlanet_semantic3d.sh
+++ scripts/train_scripts/randlanet_semantic3d.sh
@@ -11,4 +11,5 @@ fi
 
 cd ../..
 python scripts/semseg.py $1 -c ml3d/configs/randlanet_semantic3d.yml \
---dataset_path /export/share/Datasets/Semantic3D
+--dataset_path /export/share/Datasets/Semantic3D_2000 \
+--pipeline.train_sum_dir /export/share/projects/open3d_ml/training_logs/v0.11/
