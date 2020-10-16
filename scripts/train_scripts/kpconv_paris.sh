05fe689aa4207579c70359b2ab74e94e1646db23
YilingQiao <yilingq@umd.edu>
2020-10-16 10:51:55 -0400

    put back train_scripts

---
+++ scripts/train_scripts/kpconv_paris.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p gpu 
+#SBATCH --gres=gpu:1 
+
+if [ "$#" -ne 1 ]; then
+    echo "Please, provide the the training framework: torch/tf."
+    exit 1
+fi
+
+cd ../..
+python scripts/semseg.py $1 -c ml3d/configs/kpconv_parislille3d.yml \
+--dataset_path /export/share/Datasets/Paris_Lille3D
