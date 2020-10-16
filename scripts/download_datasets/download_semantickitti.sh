57858bf648b7176acbdbcebb87cb53a144c0f2c6
Benjamin Ummenhofer <benjaminum@gmail.com>
2020-10-16 22:33:19 +0200

    add instructions for downloading the velodyne data

--- scripts/download_datasets/download_semantickitti.sh
+++ scripts/download_datasets/download_semantickitti.sh
@@ -18,14 +18,26 @@ mkdir -p $BASE_DIR
 export url_data="http://www.cvlibs.net/download.php?file=data_odometry_velodyne.zip"
 export url_label="http://semantic-kitti.org/assets/data_odometry_labels.zip"
 
+
+more << EOF
+
+SemanticKITTI consists of the velodyne scans and the semantic annotation.
+The velodyne download link has to be obtained manually by visiting 
+
+http://www.cvlibs.net/download.php?file=data_odometry_velodyne.zip
+
+in your browser. After downloading, unzip the data_odometry_velodyne.zip file
+to $BASE_DIR
+
+
+press any key to continue to download and unzip the semantic labels.
+EOF
+
 wget -c -N -O $BASE_DIR'/data_odometry_labels.zip' $url_label
-wget -c -N -O $BASE_DIR'/data_odometry_velodyne.zip'  $url_data
 
 cd $BASE_DIR
 
-unzip data_odometry_velodyne.zip
 unzip data_odometry_labels.zip
 
 mkdir zip_files
 mv data_odometry_labels.zip zip_files
-mv data_odometry_velodyne.zip zip_files
