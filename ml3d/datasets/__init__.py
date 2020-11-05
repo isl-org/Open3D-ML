5645ac9ff81c636f926398694c6e63f63651a8cf
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2020-11-05 21:31:01 +0530

    Add Waymo Dataset (#136)
    * add waymo preprocess
    
    * add waymo class
    
    * added argparse
    
    * apply style
    
    * add docstring
    
    * remove cv2

--- ml3d/datasets/__init__.py
+++ ml3d/datasets/__init__.py
@@ -9,8 +9,9 @@ from .toronto3d import Toronto3D
 from .customdataset import Custom3D
 from .semantic3d import Semantic3D
 from .kitti import KITTI
+from .waymo import Waymo
 from . import utils
 __all__ = [
     'SemanticKITTI', 'S3DIS', 'Toronto3D', 'ParisLille3D', 'Semantic3D',
-    'Custom3D', 'utils', 'KITTI'
+    'Custom3D', 'utils', 'KITTI', 'Waymo'
 ]
