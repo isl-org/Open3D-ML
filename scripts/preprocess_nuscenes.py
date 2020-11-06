95b697dd09b090e221a8f675a6c62906cff09073
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2020-11-06 20:27:18 +0530

    bug fix

--- scripts/preprocess_nuscenes.py
+++ scripts/preprocess_nuscenes.py
@@ -21,7 +21,9 @@ class NuScenesProcess():
         
         assert version in ['v1.0-trainval', 'v1.0-test', 'v1.0-mini']
         self.is_test = 'test' in version
-        nusc = NuScenes(version=version, dataroot=dataset_path, verbose=True)
+        self.out_path = out_path
+
+        self.nusc = NuScenes(version=version, dataroot=dataset_path, verbose=True)
 
         if version == 'v1.0-trainval':
             train_scenes = splits.train
@@ -35,12 +37,13 @@ class NuScenesProcess():
         else:
             raise ValueError('unknown')
 
-        available_scenes = get_available_scenes(nusc)
+        self.version = version
+        available_scenes = self.get_available_scenes()
         names = [sc['name'] for sc in available_scenes]
         train_scenes = list(filter(lambda x : x in names, train_scenes))
         val_scenes = list(filter(lambda x : x in names, val_scenes))
 
-        train_scenes = set([available_scenes[names.index(s)['token'] for s in train_scenes])
+        train_scenes = set([available_scenes[names.index(s)]['token'] for s in train_scenes])
         val_scenes = set([available_scenes[names.index(s)]['token'] for s in val_scenes])
 
         if not self.is_test:
@@ -48,14 +51,30 @@ class NuScenesProcess():
         else:
             print(f"test scenes : {len(train_scenes)}")
 
-        train_info, val_info = process_scenes(nusc)
-    
-    @staticmethod
-    def process_scenes(nusc):
+        self.train_scenes = train_scenes
+        self.val_scenes = val_scenes
+
+    def convert(self):
+        train_info, val_info = self.process_scenes()
+        train_info['version'] = self.version
+        val_info['version'] = self.version
+        out_path = self.out_path
+
+        if self.is_test:
+            with open(join(out_path, 'infos_test.pkl'), 'wb') as f:
+                pickle.dump(train_info, f)
+        else:
+            with open(join(out_path, 'infos_train.pkl'), 'wb') as f:
+                pickle.dump(train_info, f)
+            with open(join(out_path, 'infos_val.pkl'), 'wb') as f:
+                pickle.dump(val_info, f)
+
+    def process_scenes(self):
+        nusc = self.nusc
         train_info = []
         val_info = []
 
-        for sample in self.sample:
+        for sample in tqdm(nusc.sample):
             lidar_token = sample['data']['LIDAR_TOP']
             sd_rec = nusc.get('sample_data', lidar_token)
             calib_rec = nusc.get('calibrated_sensor', sd_rec['calibrated_sensor_token'])
@@ -77,21 +96,31 @@ class NuScenesProcess():
 
         if not self.is_test:
             annotations = [nusc.get('sample_annotation', token) for token in sample['anns']]
-            locs = np.array([b.centre for b in boxes]).reshape(-1, 3)
+            locs = np.array([b.center for b in boxes]).reshape(-1, 3)
             dims = np.array([b.wlh for b in boxes]).reshape(-1, 3)
             rots = np.array([b.orientation.yaw_pitch_roll[0] for b in boxes]).reshape(-1, 1)
 
             valid_flag = np.array([(ann['num_lidar_pts'] + ann['num_radar_pts']) > 0 for ann in annotations]).reshape(-1)
 
             names = [b.name if b.name not in self.mapping else self.mapping[b.name] for b in boxes]
+            names = np.array(names)
+
+            gt_boxes = np.concatenate([locs, dims, -rots - np.pi/2], axis=1)
+
+            data['gt_boxes'] = gt_boxes
+            data['gt_names'] = names
+            data['num_lidar_points'] = np.array([ann['num_lidar_points'] for ann in annotations])
+            data['valid_flag'] = valid_flag
+
+        if sample['scene_token'] in self.train_scenes:
+            train_info.append(data)
+        else:
+            val_info.append(data)
         
+        return train_info, val_info
 
-
-
-
-
-    @staticmethod
-    def get_available_scenes(nusc):
+    def get_available_scenes(self):
+        nusc = self.nusc
         available_scenes = []
 
         for scene in nusc.scene:
@@ -110,3 +139,7 @@ class NuScenesProcess():
 
         return available_scenes
 
+
+if __name__ == '__main__':
+    converter = NuScenesProcess("../../data/nuscenes/", './nusc/', 'v1.0-mini')
+    converter.convert()
