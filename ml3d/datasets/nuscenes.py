410d10a01b08f4f04012967376bce2f4fec101b4
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2020-11-09 22:14:50 +0530

    added label

--- ml3d/datasets/nuscenes.py
+++ ml3d/datasets/nuscenes.py
@@ -53,16 +53,14 @@ class NuScenes(BaseDataset):
         self.val_info = {}
 
         if os.path.exists(join(info_path, 'infos_train.pkl')):
-            self.train_info = pickle.load(
-                open(join(info_path, 'infos_train.pkl'), 'rb'))
+            self.train_info = pickle.load(open(join(info_path, 'infos_train.pkl'), 'rb'))
 
         if os.path.exists(join(info_path, 'infos_val.pkl')):
-            self.val_info = pickle.load(
-                open(join(info_path, 'infos_val.pkl'), 'rb'))
+            self.val_info = pickle.load(open(join(info_path, 'infos_val.pkl'), 'rb'))
 
         if os.path.exists(join(info_path, 'infos_test.pkl')):
-            self.test_info = pickle.load(
-                open(join(info_path, 'infos_test.pkl'), 'rb'))
+            self.test_info = pickle.load(open(join(info_path, 'infos_test.pkl'), 'rb'))
+
 
     @staticmethod
     def get_label_to_names():
@@ -88,34 +86,21 @@ class NuScenes(BaseDataset):
         return np.fromfile(path, dtype=np.float32).reshape(-1, 5)
 
     @staticmethod
-    def read_calib(path):
-        assert Path(path).exists()
+    def read_label(info):
+        mask = info['num_lidar_pts'] > 0
+        boxes = info['gt_boxes'][mask]
+        names = info['gt_names'][mask]
 
-        with open(path, 'r') as f:
-            lines = f.readlines()
-        obj = lines[2].strip().split(' ')[1:]
-        P2 = np.array(obj, dtype=np.float32)
-
-        obj = lines[3].strip().split(' ')[1:]
-        P3 = np.array(obj, dtype=np.float32)
-
-        obj = lines[4].strip().split(' ')[1:]
-        R0 = np.array(obj, dtype=np.float32)
-
-        obj = lines[5].strip().split(' ')[1:]
-        Tr_velo_to_cam = np.array(obj, dtype=np.float32)
-
-        return {
-            'P2': P2.reshape(3, 4),
-            'P3': P3.reshape(3, 4),
-            'R0': R0.reshape(3, 3),
-            'Tr_velo2cam': Tr_velo_to_cam.reshape(3, 4)
-        }
+        objects = []
+        for name, box in zip(names, boxes):
+            objects.append(Object3d(name, box))
+        
+        return objects
 
     def get_split(self, split):
         return NuSceneSplit(self, split=split)
 
-    def get_split_list(self, split):
+    def get_split_list(self, split):        
         if split in ['train', 'training']:
             return self.train_info
         elif split in ['test', 'testing']:
@@ -150,19 +135,30 @@ class NuSceneSplit():
     def get_data(self, idx):
         info = self.infos[idx]
         lidar_path = info['lidar_path']
+        print(info.keys())
 
         pc = self.dataset.read_lidar(lidar_path)
+        label = self.dataset.read_label(info)
+
+        calib = {
+            'lidar2ego_tr': info['lidar2ego_tr'],
+            'lidar2ego_rot': info['lidar2ego_rot'],
+            'ego2global_tr': info['ego2global_tr'],
+            'ego2global_rot': info['ego2global_rot'],
+        }
 
         data = {
             'point': pc,
+            'feat': None,
+            'calib': calib,
+            'label': label,
         }
-
-        # print(info.keys())
 
         return data
 
     def get_attr(self, idx):
-        pc_path = self.path_list[idx]
+        info = self.infos[idx]
+        pc_path = info['lidar_path']
         name = Path(pc_path).name.split('.')[0]
 
         attr = {'name': name, 'path': pc_path, 'split': self.split}
@@ -171,62 +167,42 @@ class NuSceneSplit():
 
 class Object3d(object):
     """
-    Stores object specific details like bbox coordinates, occlusion etc.
+    Stores object specific details like bbox coordinates.
     """
 
-    def __init__(self, line):
-        label = line.strip().split(' ')
-        self.src = line
-        self.cls_type = label[0]
+    def __init__(self, name, box):
+        self.cls_type = name
         self.cls_id = self.cls_type_to_id(self.cls_type)
-        self.truncation = float(label[1])
-        self.occlusion = float(
-            label[2]
-        )  # 0:fully visible 1:partly occluded 2:largely occluded 3:unknown
-        self.alpha = float(label[3])
-        self.box2d = np.array((float(label[4]), float(label[5]), float(
-            label[6]), float(label[7])),
-                              dtype=np.float32)
-        self.h = float(label[8])
-        self.w = float(label[9])
-        self.l = float(label[10])
+        self.h = float(box[5])
+        self.w = float(box[3])
+        self.l = float(box[4])
         self.loc = np.array(
-            (float(label[11]), float(label[12]), float(label[13])),
+            (float(box[0]), float(box[1]), float(box[2])),
             dtype=np.float32)
         self.dis_to_cam = np.linalg.norm(self.loc)
-        self.ry = float(label[14])
-        self.score = float(label[15]) if label.__len__() == 16 else -1.0
-        self.level_str = None
-        self.level = self.get_kitti_obj_level()
+        self.ry = float(box[6])
 
     @staticmethod
     def cls_type_to_id(cls_type):
         """
         get object id from name.
         """
-        type_to_id = {'Car': 1, 'Pedestrian': 2, 'Cyclist': 3, 'Van': 4}
+        type_to_id = {
+            'ignore': 0,
+            'barrier': 1,
+            'bicycle': 2,
+            'bus': 3,
+            'car': 4,
+            'construction_vehicle': 5,
+            'motorcycle': 6,
+            'pedestrian': 7,
+            'traffic_cone': 8,
+            'trailer': 9,
+            'truck': 10,
+            }
         if cls_type not in type_to_id.keys():
             return -1
         return type_to_id[cls_type]
-
-    def get_kitti_obj_level(self):
-        """
-        determines the difficulty level of object.
-        """
-        height = float(self.box2d[3]) - float(self.box2d[1]) + 1
-
-        if height >= 40 and self.truncation <= 0.15 and self.occlusion <= 0:
-            self.level_str = 'Easy'
-            return 0  # Easy
-        elif height >= 25 and self.truncation <= 0.3 and self.occlusion <= 1:
-            self.level_str = 'Moderate'
-            return 1  # Moderate
-        elif height >= 25 and self.truncation <= 0.5 and self.occlusion <= 2:
-            self.level_str = 'Hard'
-            return 2  # Hard
-        else:
-            self.level_str = 'UnKnown'
-            return -1
 
     def generate_corners3d(self):
         """
@@ -245,19 +221,6 @@ class Object3d(object):
         corners3d = np.dot(R, corners3d).T
         corners3d = corners3d + self.loc
         return corners3d
-
-    def to_str(self):
-        print_str = '%s %.3f %.3f %.3f box2d: %s hwl: [%.3f %.3f %.3f] pos: %s ry: %.3f' \
-                     % (self.cls_type, self.truncation, self.occlusion, self.alpha, self.box2d, self.h, self.w, self.l,
-                        self.loc, self.ry)
-        return print_str
-
-    def to_kitti_format(self):
-        kitti_str = '%s %.2f %d %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f' \
-                    % (self.cls_type, self.truncation, int(self.occlusion), self.alpha, self.box2d[0], self.box2d[1],
-                       self.box2d[2], self.box2d[3], self.h, self.w, self.l, self.loc[0], self.loc[1], self.loc[2],
-                       self.ry)
-        return kitti_str
 
 
 DATASET._register_module(NuScenes)
