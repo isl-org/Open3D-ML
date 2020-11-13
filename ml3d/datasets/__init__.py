227e35b526888879e860aa970e8b17da24a4c3ae
Matthias Humt <matthias.humt@dlr.de>
2020-11-13 16:22:02 +0100

    Added PointNet model and ShapeNet dataset

--- ml3d/datasets/__init__.py
+++ ml3d/datasets/__init__.py
@@ -8,8 +8,9 @@ from .parislille3d import ParisLille3D
 from .toronto3d import Toronto3D
 from .customdataset import Custom3D
 from .semantic3d import Semantic3D
+from .shapenet import ShapeNet
 from . import utils
 __all__ = [
     'SemanticKITTI', 'S3DIS', 'Toronto3D', 'ParisLille3D', 'Semantic3D',
-    'Custom3D', 'utils'
+    'Custom3D', 'ShapeNet', 'utils'
 ]
