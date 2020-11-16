fb9024b19214d722e37fcdb0ad83ed784e099953
Geoffrey Prewett <prewettg@gmail.com>
2020-11-16 15:49:16 -0800

    Added documentation, changed definition of size argument to be edge-to-edge and adjusted code accordingly

--- ml3d/vis/boundingbox.py
+++ ml3d/vis/boundingbox.py
@@ -3,6 +3,8 @@ import open3d as o3d
 
 
 class BoundingBox3D:
+    """Class that defines an axially-oriented bounding box."""
+
     next_id = 1
 
     def __init__(self,
@@ -19,6 +21,29 @@ class BoundingBox3D:
                  show_meta=None,
                  identifier=None,
                  arrow_length=1.0):
+        """Creates a bounding box. Front, up, left define the axis of the box
+        and must be normalized and mutually orthogonal.
+
+        center: (x, y, z) that defines the center of the box
+        front: normalized (i, j, k) that defines the front direction of the box
+        up: normalized (i, j, k) that defines the up direction of the box
+        left: normalized (i, j, k) that defines the left direction of the box
+        size: (width, height, depth) that defines the size of the box, as
+            measured from edge to edge
+        label_class: integer specifying the classification label. If an LUT is
+            specified in create_lines() this will be used to determine the color
+            of the box.
+        confidence: confidence level of the box
+        meta: a user-defined string (optional)
+        show_class: displays the class label in text near the box (optional)
+        show_confidence: displays the confidence value in text near the box
+            (optional)
+        show_meta: displays the meta string in text near the box (optional)
+        identifier: a unique integer that defines the id for the box (optional,
+            will be generated if not provided)
+        arrow_length: the length of the arrow in the front_direct. Set to zero
+            to disable the arrow (optional)
+        """
         assert (len(center) == 3)
         assert (len(front) == 3)
         assert (len(up) == 3)
@@ -52,7 +77,15 @@ class BoundingBox3D:
         return s
 
     @staticmethod
-    def create_lines(boxes, lut):
+    def create_lines(boxes, lut=None):
+        """Creates and returns an open3d.geometry.LineSet that can be used to
+        render the boxes.
+
+        boxes: the list of bounding boxes
+        lut: a ml3d.vis.LabelLUT that is used to look up the color based on the
+            label_class argument of the BoundingBox3D constructor. If not
+            provided, a color of 50% grey will be used. (optional)
+        """
         nverts = 14
         nlines = 17
         points = np.zeros((nverts * len(boxes), 3), dtype="float32")
@@ -62,9 +95,9 @@ class BoundingBox3D:
         for i in range(0, len(boxes)):
             box = boxes[i]
             pidx = nverts * i
-            x = box.size[0] * box.left
-            y = box.size[1] * box.up
-            z = box.size[2] * box.front
+            x = 0.5 * box.size[0] * box.left
+            y = 0.5 * box.size[1] * box.up
+            z = 0.5 * box.size[2] * box.front
             arrow_tip = box.center + z + box.arrow_length * box.front
             arrow_mid = box.center + z + 0.60 * box.arrow_length * box.front
             head_length = 0.3 * box.arrow_length
