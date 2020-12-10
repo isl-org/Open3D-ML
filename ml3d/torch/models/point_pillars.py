fixed bug in loss calculation, fixed bug in save logs

ml3d/torch/models/point_pillars.py
@@ -156,11 +156,11 @@ class PointPillars(BaseModel):
         target_idx = target_idx[cond]
         target_bboxes = target_bboxes[cond]
 
+        bboxes = bboxes.permute((0, 2, 3, 1)).reshape(-1, self.bbox_head.box_code_size)[pos_idx]
+        dirs = dirs.permute((0, 2, 3, 1)).reshape(-1, 2)[pos_idx]
+
         if len(pos_idx) > 0:
             # direction classification loss
-            bboxes = bboxes.permute((0, 2, 3, 1)).reshape(-1, self.bbox_head.box_code_size)[pos_idx]
-            dirs = dirs.permute((0, 2, 3, 1)).reshape(-1, 2)[pos_idx]
-
             # to discrete bins
             target_dirs = torch.cat(gt_bboxes, axis=0)[target_idx][:, -1]
             target_dirs = limit_period(target_dirs, 0, 2 * np.pi)

ml3d/torch/pipelines/object_detection.py
@@ -131,7 +131,10 @@ class ObjectDetection(BasePipeline):
         self.valid_mAP = {}
 
         with torch.no_grad():
+            i = 0
             for inputs in tqdm(valid_loader, desc='validation'):
+                if i == 5: break
+                i+=1
                 results = model(inputs['data']['point'].to(self.device))
                 loss = model.loss(results, inputs['data'])
                 for l, v in loss.items():
@@ -198,7 +201,10 @@ class ObjectDetection(BasePipeline):
 
             self.losses = {}
             process_bar = tqdm(train_loader, desc='training')        
+            i = 0
             for inputs in process_bar:
+                if i == 5: break
+                i+=1
                 results = model(inputs['data']['point'].to(self.device))
                 loss = model.loss(results, inputs['data'])
                 loss_sum = sum(loss.values())
@@ -232,10 +238,10 @@ class ObjectDetection(BasePipeline):
 
     def save_logs(self, writer, epoch):
         for key, val in self.losses.items():
-            writer.add_scalar("train/"+key, val, epoch)
+            writer.add_scalar("train/"+key, np.mean(val), epoch)
 
         for key, val in self.valid_losses.items():
-            writer.add_scalar("valid/"+key, val, epoch)
+            writer.add_scalar("valid/"+key, np.mean(val), epoch)
 
 
     def load_ckpt(self, ckpt_path=None, is_resume=True):