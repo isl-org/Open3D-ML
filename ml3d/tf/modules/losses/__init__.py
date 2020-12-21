26819babc2402241be713ff7447b75d791962e68
AbhishekS <65679171+ClarytyLLC@users.noreply.github.com>
2020-12-21 14:08:52 +0530

    Update __init__.py

--- ml3d/tf/modules/losses/__init__.py
+++ ml3d/tf/modules/losses/__init__.py
@@ -3,5 +3,4 @@ from .cross_entropy import CrossEntropyLoss
 from .focal_loss import FocalLoss
 from .smooth_L1 import SmoothL1Loss
 
-
 __all__ = ['SemSegLoss', 'CrossEntropyLoss', 'FocalLoss', 'SmoothL1Loss']
