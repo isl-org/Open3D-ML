64c4111d4b8460db0b9036ccfae2965039a64787
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2021-01-26 19:48:50 +0530

    modify download script

--- scripts/download_datasets/download_sunrgbd.sh
+++ scripts/download_datasets/download_sunrgbd.sh
@@ -15,32 +15,20 @@ BASE_DIR="$1"/sunrgbd
 
 mkdir -p ${BASE_DIR}
 
-mkdir -p ${BASE_DIR}/OFFICIAL_SUNRGBD
-
 url_sunrgbd="http://rgbd.cs.princeton.edu/data/SUNRGBD.zip"
 url_2dbb="http://rgbd.cs.princeton.edu/data/SUNRGBDMeta2DBB_v2.mat"
 url_3dbb="http://rgbd.cs.princeton.edu/data/SUNRGBDMeta3DBB_v2.mat"
 url_toolbox="http://rgbd.cs.princeton.edu/data/SUNRGBDtoolbox.zip"
 
-wget -c -N -O ${BASE_DIR}'/OFFICIAL_SUNRGBD/SUNRGBD.zip' ${url_sunrgbd} --no-check-certificate
-wget -c -N -O ${BASE_DIR}'/OFFICIAL_SUNRGBD/SUNRGBDMeta2DBB_v2.mat' ${url_2dbb} --no-check-certificate
-wget -c -N -O ${BASE_DIR}'/OFFICIAL_SUNRGBD/SUNRGBDMeta3DBB_v2.mat' ${url_3dbb} --no-check-certificate
-wget -c -N -O ${BASE_DIR}'/OFFICIAL_SUNRGBD/SUNRGBDtoolbox.zip' ${url_toolbox} --no-check-certificate
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+wget -c -N -O ${BASE_DIR}'/SUNRGBD.zip' ${url_sunrgbd} --no-check-certificate
+wget -c -N -O ${BASE_DIR}'/SUNRGBDMeta2DBB_v2.mat' ${url_2dbb} --no-check-certificate
+wget -c -N -O ${BASE_DIR}'/SUNRGBDMeta3DBB_v2.mat' ${url_3dbb} --no-check-certificate
+wget -c -N -O ${BASE_DIR}'/SUNRGBDtoolbox.zip' ${url_toolbox} --no-check-certificate
 
 cd ${BASE_DIR}
-
-mkdir matlab
-
-cp ${SCRIPT_DIR}/matlab/* ./matlab
-
-cd OFFICIAL_SUNRGBD
 
 unzip SUNRGBD.zip
 unzip SUNRGBDtoolbox.zip
 
 mkdir -p ${BASE_DIR}/zip_files
 mv SUNRGBDtoolbox.zip SUNRGBD.zip ${BASE_DIR}/zip_files
-
-echo "Please run all three matlab files present in " \"${BASE_DIR}/matlab\" "to extract data"
