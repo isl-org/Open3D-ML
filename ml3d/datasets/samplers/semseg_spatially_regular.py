2a04231df0be39e2b8030e480d342cee5574fb9a
eiiijiiiy <68719911+eiiijiiiy@users.noreply.github.com>
2021-05-04 20:15:42 +0800

    Update semseg_spatially_regular.py

--- ml3d/datasets/samplers/semseg_spatially_regular.py
+++ ml3d/datasets/samplers/semseg_spatially_regular.py
@@ -82,8 +82,10 @@ class SemSegSpatiallyRegularSampler(object):
                     idxs = search_tree.query_radius(center_point, r=radius)[0]
                 elif num_points is not None:
                     if (pc.shape[0] < num_points):
+                        diff = num_points - pc.shape[0]
                         idxs = np.array(range(pc.shape[0]))
                         idxs = list(idxs) + list(random.choices(idxs, k=diff))
+                        idxs = np.asarray(idxs)
                     else:
                         idxs = search_tree.query(center_point,
                                                  k=num_points)[1][0]
