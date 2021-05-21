651e904601914bf2900194604eababa1b088784a
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2021-05-21 19:08:02 +0530

    fic batch size

--- ml3d/torch/pipelines/semantic_segmentation.py
+++ ml3d/torch/pipelines/semantic_segmentation.py
@@ -208,7 +208,7 @@ class SemanticSegmentation(BasePipeline):
                                      sampler=test_sampler,
                                      use_cache=dataset.cfg.use_cache)
         test_loader = DataLoader(test_split,
-                                 batch_size=cfg.batch_size,
+                                 batch_size=cfg.test_batch_size,
                                  sampler=get_sampler(test_sampler),
                                  collate_fn=batcher.collate_fn)
 
