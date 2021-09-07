763e6db3778e448ec3fd5723efefe476bfd44eac
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2021-09-07 18:12:25 +0530

    add hue saturation augmentation

--- ml3d/datasets/augment/augmentation.py
+++ ml3d/datasets/augment/augmentation.py
@@ -263,6 +263,98 @@ class SemsegAugmentation(Augmentation):
             feats[:, :3] = np.clip(noise + feats[:, :3], 0, 255)
         return feats
 
+    @staticmethod
+    def __rgb_to_hsv(rgb):
+        """Converts RGB to HSV.
+
+        Translated from source of colorsys.rgb_to_hsv
+        r,g,b should be a numpy arrays with values between 0 and 255
+        rgb_to_hsv returns an array of floats between 0.0 and 1.0.
+
+        Args:
+            rgb: RGB image
+
+        Returns:
+            HSV image
+
+        """
+        rgb = rgb.astype('float')
+        hsv = np.zeros_like(rgb)
+        # in case an RGBA array was passed, just copy the A channel
+        hsv[..., 3:] = rgb[..., 3:]
+        r, g, b = rgb[..., 0], rgb[..., 1], rgb[..., 2]
+        maxc = np.max(rgb[..., :3], axis=-1)
+        minc = np.min(rgb[..., :3], axis=-1)
+        hsv[..., 2] = maxc
+        mask = maxc != minc
+        hsv[mask, 1] = (maxc - minc)[mask] / maxc[mask]
+        rc = np.zeros_like(r)
+        gc = np.zeros_like(g)
+        bc = np.zeros_like(b)
+        rc[mask] = (maxc - r)[mask] / (maxc - minc)[mask]
+        gc[mask] = (maxc - g)[mask] / (maxc - minc)[mask]
+        bc[mask] = (maxc - b)[mask] / (maxc - minc)[mask]
+        hsv[..., 0] = np.select([r == maxc, g == maxc],
+                                [bc - gc, 2.0 + rc - bc],
+                                default=4.0 + gc - rc)
+        hsv[..., 0] = (hsv[..., 0] / 6.0) % 1.0
+
+        return hsv
+
+    @staticmethod
+    def __hsv_to_rgb(hsv):
+        """Converts HSV to RGB.
+
+        Translated from source of colorsys.hsv_to_rgb
+        h,s should be a numpy arrays with values between 0.0 and 1.0
+        v should be a numpy array with values between 0.0 and 255.0
+        hsv_to_rgb returns an array of uints between 0 and 255.
+
+        Args:
+            hsv: HSV image
+
+        Returns:
+            RGB image
+
+        """
+        rgb = np.empty_like(hsv)
+        rgb[..., 3:] = hsv[..., 3:]
+        h, s, v = hsv[..., 0], hsv[..., 1], hsv[..., 2]
+        i = (h * 6.0).astype('uint8')
+        f = (h * 6.0) - i
+        p = v * (1.0 - s)
+        q = v * (1.0 - s * f)
+        t = v * (1.0 - s * (1.0 - f))
+        i = i % 6
+        conditions = [s == 0.0, i == 1, i == 2, i == 3, i == 4, i == 5]
+        rgb[..., 0] = np.select(conditions, [v, q, p, p, t, v], default=v)
+        rgb[..., 1] = np.select(conditions, [v, v, v, q, p, p], default=t)
+        rgb[..., 2] = np.select(conditions, [v, p, t, v, v, q], default=p)
+
+        return rgb.astype('uint8')
+
+    @staticmethod
+    def HueSaturationTranslation(feat, cfg):
+        """Adds small noise to hue and saturation.
+
+        Args:
+            feat: Features.
+            cfg: configuration dict.
+
+        """
+        hue_max = cfg.get('hue_max', 0.5)
+        saturation_max = cfg.get('saturation_max', 0.2)
+
+        # Assume feat[:, :3] is rgb
+        hsv = SemsegAugmentation.__rgb_to_hsv(feat[:, :3])
+        hue_val = (np.random.rand() - 0.5) * 2 * hue_max
+        sat_ratio = 1 + (np.random.rand() - 0.5) * 2 * saturation_max
+        hsv[..., 0] = np.remainder(hue_val + hsv[..., 0] + 1, 1)
+        hsv[..., 1] = np.clip(sat_ratio * hsv[..., 1], 0, 1)
+        feat[:, :3] = np.clip(SemsegAugmentation.__hsv_to_rgb(hsv), 0, 255)
+
+        return feat
+
     def augment(self, point, feat, labels, cfg):
         if cfg is None:
             return point, feat, labels
@@ -300,6 +392,10 @@ class SemsegAugmentation(Augmentation):
 
         if 'ChromaticJitter' in cfg:
             feat = self.ChromaticJitter(feat, cfg['ChromaticJitter'])
+
+        if 'HueSaturationTranslation' in cfg:
+            feat = self.HueSaturationTranslation(
+                feat, cfg['HueSaturationTranslation'])
 
         return point, feat, labels
 
