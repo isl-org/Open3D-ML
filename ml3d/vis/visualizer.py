556a6b6fe892ef251126683979f129749e732681
Ajinkya <khoche@kth.se>
2021-09-22 19:03:14 +0200

    add self.bounding_box_data to DataModel, if userdata has bounding_boxes key

--- ml3d/vis/visualizer.py
+++ ml3d/vis/visualizer.py
@@ -220,6 +220,7 @@ class DataModel(Model):
         # We could just create the TPointCloud here, but that would cause the UI
         # to block. If we do it on load then the loading dialog will display.
         self._name2srcdata = {}
+        self.bounding_box_data = []
         for d in userdata:
             name = d["name"]
             while name in self._data:  # ensure each name is unique
@@ -227,10 +228,14 @@ class DataModel(Model):
             self._init_data(name)
             self._name2srcdata[name] = d
 
+            if 'bounding_boxes' in d:
+                self.bounding_box_data.append(
+                    Model.BoundingBoxData(name, d['bounding_boxes']))
+
     def load(self, name, fail_if_no_space=False):
         """Load a pointcloud based on the name provided."""
         if self.is_loaded(name):
-            return
+            return True
 
         self.create_point_cloud(self._name2srcdata[name])
 
