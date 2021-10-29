c0edaa3588c1827209c6330ddce0e240fa44f9ed
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2021-10-29 18:15:26 +0530

    Added Point Transformers (#327)
    * add pytorch point_transformer
    
    * fix knn cuda
    
    * fix sampling
    
    * fix index
    
    * use cpu knn
    
    * add augment
    
    * added tf model
    
    * style
    
    * tf 2.5 compatible
    
    * fix knn
    
    * fix bn
    
    * add inference
    
    * add docstring
    
    * use ml knn
    
    * fix memory leak
    
    * address lgtm
    
    * fix style
    
    * update config
    
    * fix knn bug
    
    * Update Readme
    
    * address reviews
    
    * type
    
    * apply-style
    
    * add hue saturation augmentation
    
    * change params
    
    * change optimizer
    
    * remove comments
    
    * fix dimension
    
    * change optim
    
    * address reviews
    
    * Update Readme
    
    * fix pydocstyle issues

--- ml3d/tf/utils/pointnet/pointnet2_utils.py
+++ ml3d/tf/utils/pointnet/pointnet2_utils.py
@@ -25,6 +25,33 @@ def furthest_point_sample(xyz, npoint):
 ops.NoGradient('Open3DFurthestPointSampling')
 
 
+def furthest_point_sample_v2(xyz, row_splits, new_row_splits):
+    """Furthest Point Sampling with variable length batch support.
+
+    Args:
+        xyz (tf.float32): Input pointcloud (N, 3).
+        row_splits (tf.int64): splits to define batch (b + 1,)
+        new_row_splits (tf.int64): splits for output batch lengths (b + 1,)
+
+    Returns:
+        Returns indices of sampled points with shape (new_row_splits[-1], ).
+    """
+    if not open3d.core.cuda.device_count() > 0:
+        raise NotImplementedError
+
+    idx = []
+    for i in range(tf.shape(row_splits)[0] - 1):
+        npoint = new_row_splits[i + 1] - new_row_splits[i]
+        start_i = row_splits[i]
+        end_i = row_splits[i + 1]
+        out = ml_ops.furthest_point_sampling(
+            tf.expand_dims(xyz[start_i:end_i], 0), npoint) + row_splits[i]
+
+        idx.append(out[0])
+
+    return tf.concat(idx, 0)
+
+
 def three_nn_gpu(query_pts, data_pts):
     """Find the three nearest neighbors of query_pts in data_pts.
 
