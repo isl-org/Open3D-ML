d6394335842cccc9ab05484cf37296d04661c48f
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2021-11-05 15:43:34 +0530

    fix style

--- ml3d/torch/modules/metrics/__init__.py
+++ ml3d/torch/modules/metrics/__init__.py
@@ -1,5 +1,6 @@
 """Metrics for Semantic Segmentation.
-Includes accuracy, mIoU and confusion matrix."""
+Includes accuracy, mIoU and confusion matrix.
+"""
 
 from .semseg_metric import SemSegMetric
 
