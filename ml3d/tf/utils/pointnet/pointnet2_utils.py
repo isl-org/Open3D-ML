86de1a170ce29592f2f14028557501808ae2a29f
Hristo Vassilev <90904094+biophase@users.noreply.github.com>
2022-04-14 18:31:34 +0300

    ml_ops fix
    fixed a wrong call to furthest_point_sampling in the module open3d.ml.tf.ops

--- ml3d/tf/utils/pointnet/pointnet2_utils.py
+++ ml3d/tf/utils/pointnet/pointnet2_utils.py
@@ -44,7 +44,7 @@ def furthest_point_sample_v2(xyz, row_splits, new_row_splits):
         npoint = new_row_splits[i + 1] - new_row_splits[i]
         start_i = row_splits[i]
         end_i = row_splits[i + 1]
-        out = ml_ops.furthest_point_sampling(
+        out = furthest_point_sampling(
             tf.expand_dims(xyz[start_i:end_i], 0), npoint) + row_splits[i]
 
         idx.append(out[0])
