bc9c6fc938c0e683a151401ea7e14c6f1c0fd110
Narayanan E.R <narayananr@iiitd.ac.in>
2022-09-15 04:09:00 -0700

    Removing duplicate variable names

--- ml3d/torch/models/pvrcnn_plus_plus.py
+++ ml3d/torch/models/pvrcnn_plus_plus.py
@@ -152,17 +152,12 @@ class PVRCNNPlusPlus(BaseModel):
                                                           _gt_boxes=gt_boxes,
                                                           _gt_labels=gt_labels,
                                                           roi_labels=roi_labels)
-        rois = targets_dict['rois']
-        roi_scores = targets_dict['roi_scores']
-        roi_labels = targets_dict['roi_labels']
-        with torch.no_grad():
-            keypoints = self.voxel_set_abstraction.get_keypoints(inputs, rois, len(gt_boxes))
         # if keypoints.shape[0] == 0 or keypoints.shape[0] == 1:
         #     return None, None, None
-        if False: # targets_dict is not None:
-            rois = targets_dict['rois']
-            roi_scores = targets_dict['roi_scores']
-            roi_labels = targets_dict['roi_labels']
+        if targets_dict is not None:
+            rois_new = targets_dict['rois']
+            roi_scores_new = targets_dict['roi_scores']
+            roi_labels_new = targets_dict['roi_labels']
             roi_targets_dict = targets_dict
         else:
             print("TARGETS_DICT WAS NONE")
@@ -175,7 +170,7 @@ class PVRCNNPlusPlus(BaseModel):
             return return_targets_dict
 
         point_features, point_coords, point_features_before_fusion = self.voxel_set_abstraction(
-            inputs, rois, x_bev_2d, x_intermediate_layers)
+            inputs, rois_new, x_bev_2d, x_intermediate_layers)
         # del (x_bev_2d)
         # torch.cuda.empty_cache()
 
@@ -183,9 +178,9 @@ class PVRCNNPlusPlus(BaseModel):
             print("KEYPOINTS ARE ONLY 1")
             self.keypoints_not_found = True
             return_targets_dict = {}
-            return_targets_dict['batch_cls_preds'] = roi_scores
-            return_targets_dict['batch_box_preds'] = rois
-            return_targets_dict['roi_labels'] = roi_labels
+            return_targets_dict['batch_cls_preds'] = roi_scores_new
+            return_targets_dict['batch_box_preds'] = rois_new
+            return_targets_dict['roi_labels'] = roi_labels_new
             return_targets_dict['cls_preds_normalized'] = False
             return return_targets_dict
 
@@ -193,8 +188,8 @@ class PVRCNNPlusPlus(BaseModel):
             len(gt_boxes), point_features, point_coords,
             point_features_before_fusion, gt_boxes, gt_labels)
 
-        outs = self.box_refinement(point_coords, point_features, rois,
-                                   roi_scores, roi_labels, keypoint_weights,
+        outs = self.box_refinement(point_coords, point_features, rois_new,
+                                   roi_scores_new, roi_labels_new, keypoint_weights,
                                    roi_targets_dict, gt_boxes, gt_labels)
 
         return outs
