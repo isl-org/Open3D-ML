e031b05db6c3173c88677abea01fa29aa7fcc0e5
john-philipp <>
2024-12-09 14:56:30 +0100

    Allowing for features (e.g. intensity) for semantickitti datasets. Needs in_channels >= 4 in config.

--- ml3d/datasets/semantickitti.py
+++ ml3d/datasets/semantickitti.py
@@ -282,7 +282,7 @@ class SemanticKITTISplit(BaseDatasetSplit):
 
         data = {
             'point': points[:, 0:3],
-            'feat': None,
+            'feat': points[:, 3:],
             'label': labels,
         }
 
